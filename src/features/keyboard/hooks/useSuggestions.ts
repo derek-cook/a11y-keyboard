ea65ad41265e39d4ff2163b6a2dae85330016106
derek-cook <dcook693@gmail.com>
2024-11-03 18:58:57 -0800

    working top logprobs strategy

--- src/features/keyboard/hooks/useSuggestions.ts
+++ src/features/keyboard/hooks/useSuggestions.ts
@@ -5,9 +5,7 @@ import { useDebouncedValue } from "~/lib/hooks/useDebouncedValue";
 export const useSuggestions = ({ text }: { text: string }) => {
   const debouncedText = useDebouncedValue(text, 1000);
 
-  const { data } = useQuery<{
-    choices: { message: { content: string } }[];
-  }>({
+  const { data } = useQuery<GetTextCompletionResponse>({
     queryKey: ["suggestions", debouncedText],
     queryFn: (): Promise<GetTextCompletionResponse> =>
       fetch(`/api/text-completion`, {
@@ -17,11 +15,5 @@ export const useSuggestions = ({ text }: { text: string }) => {
     enabled: !!debouncedText,
   });
 
-  const choices = data?.choices.map(
-    (choice) => choice.message.content.split(" ")[0],
-  );
-  const topChoices = choices ? choices.slice(0, 3) : [];
-
-  console.log({ data });
-  return { suggestions: topChoices };
+  return { suggestions: data?.slice(0, 3) ?? [] };
 };
